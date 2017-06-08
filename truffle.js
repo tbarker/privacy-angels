1895d805335501ee0e9fe61bd7385382e9002929
Thomas <git@thomasbarker.com>
2017-06-08 14:44:19 +0100

    Embedded TestRPC

--- truffle.js
+++ truffle.js
@@ -1,6 +1,12 @@
+var TestRPC = require("ethereumjs-testrpc");
+
 module.exports = {
   networks: {
     development: {
+      provider: TestRPC.provider(),
+      network_id: "*" // Match any network id
+    },
+    local: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
